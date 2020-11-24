39b314bfa98a5cba4d0be1e3e47271bc989ccf87
Farhan Munir <munir.farhan@gmail.com>
2020-11-24 19:10:36 +0500

    Added support files

---
+++ config.py
@@ -0,0 +1,10 @@
+config = {}
+
+#Preferred region for launch
+config['region'] = 'us-east-1'	
+
+#Should we log the data
+config['logging'] = True
+
+#Location of AWS Credentials
+config['aws_creds_location'] = "/home/ronin1770/.aws/credentials"
